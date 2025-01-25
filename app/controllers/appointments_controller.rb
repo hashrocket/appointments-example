210ef3e81c82b9571d68f9697677ab16e6abb2ed
Andrew Vogel <andrew.vogel@hashrocket.com>
2025-01-25 14:05:44 -0500

    feat: add edit/update views for appointment
    we are not checking who is allowed to edit in the appointment partial
    yet

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,12 +1,36 @@
 class AppointmentsController < ApplicationController
-  helper_method :appointments
+  helper_method :appointments, :appointment
 
-  def index
+  before_action :check_created_by, only: [ :edit, :update ]
+
+  def update
+    if appointment.update(appointment_params)
+      redirect_to appointments_path, notice: "Appointment updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
+  def check_created_by
+    unless appointment.created_by?(Current.user)
+      redirect_to appointments_path, alert: "Can't modify appointment - you are not the creator."
+    end
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(
+      :requested_datetime,
+      :notes
+    )
+  end
+
+  def appointment
+    @appointment ||= Appointment.find(params[:id])
+  end
+
   def appointments
-    @appointments ||= Appointment.all
+    @appointments ||= Appointment.includes(:created_by).all
   end
 end
