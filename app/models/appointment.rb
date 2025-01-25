bb82cbf195ab8767a28ca31493f6b853d4503612
Andrew Vogel <andrew.vogel@hashrocket.com>
2025-01-25 14:05:44 -0500

    feat: add edit/update views for appointment
    we are not checking who is allowed to edit in the appointment partial
    yet

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :created_by, foreign_key: :created_by_user_id, class_name: "User"
 
   validates :requested_datetime, presence: true
+
+  def created_by?(user)
+    created_by == user
+  end
 end
